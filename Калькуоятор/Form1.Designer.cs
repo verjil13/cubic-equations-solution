57ed363b9a4520e26e2cb2bfba6ff04f9fe2cc77
Verjil <13verjil@gmail.com>
2019-08-21 07:45:28 +0400

    Добавьте файлы проекта.

---
+++ Калькуоятор/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Калькуоятор
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(296, 10);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x^3+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "x^2+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "x";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(420, 10);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "0";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 82);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(127, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(169, 82);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(121, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "+ i";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(145, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "+ i";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(169, 108);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(121, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(12, 108);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(127, 20);
+            this.textBox8.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(145, 137);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "+ i";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(169, 134);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(121, 20);
+            this.textBox9.TabIndex = 15;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(12, 134);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(127, 20);
+            this.textBox10.TabIndex = 14;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(18, 170);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 263);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(306, 170);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(271, 263);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 447);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Калькуоятор/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Калькуоятор
+{
+    public partial class Form1 : Form
+    {
+        double a, b, c, d, x1, x2, x3, ix1, ix2, ix3, R, Q, S, phi,DD,xmin,xmax,ymin,ymax,x,y,x0,y0,i,aa,bb,cc,dd,yy;
+
+        
+
+        public Form1()
+         {
+            InitializeComponent();
+         }
+
+
+        public void Point(double x, double y)
+         {
+            int fx, fy; Color c = Color.Black;
+            fx = Convert.ToInt32(Math.Round((x - xmin) * pictureBox1.Width / Math.Abs(xmax - xmin)));
+            fy = Convert.ToInt32(Math.Round(pictureBox1.Height - (y - ymin) * pictureBox1.Height / Math.Abs(ymax - ymin)));
+            Graphics g = pictureBox1.CreateGraphics();
+            g.FillRectangle(new SolidBrush(c), new Rectangle(fx, fy, 1, 1));
+         }
+        public void Point1(double x, double y)
+         {
+            int fx, fy; Color c = Color.Red;
+            fx = Convert.ToInt32(Math.Round((x - xmin) * pictureBox1.Width / Math.Abs(xmax - xmin)));
+            fy = Convert.ToInt32(Math.Round(pictureBox1.Height - (y - ymin) * pictureBox1.Height / Math.Abs(ymax - ymin)));
+
+            Graphics g = pictureBox1.CreateGraphics();
+            g.FillRectangle(new SolidBrush(c), new Rectangle(fx, fy, 1, 1));
+            
+        }
+
+
+        public void Point2(double x, double y)
+        {
+            int fx, fy; Color c = Color.Black;
+            fx = Convert.ToInt32(Math.Round((x - xmin) * pictureBox2.Width / Math.Abs(xmax - xmin)));
+            fy = Convert.ToInt32(Math.Round(pictureBox1.Height - (y - ymin) * pictureBox2.Height / Math.Abs(ymax - ymin)));
+
+            Graphics g = pictureBox2.CreateGraphics();
+            g.FillRectangle(new SolidBrush(c), new Rectangle(fx, fy, 1, 1));
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Image = null;
+
+
+            d = Convert.ToDouble(textBox1.Text);
+            a = Convert.ToDouble(textBox2.Text);
+            b = Convert.ToDouble(textBox3.Text);
+            c = Convert.ToDouble(textBox4.Text);
+
+
+            dd = 3 * d;
+            aa = 2 * a;
+            bb = b;
+
+
+            if (d==0 && a==0) {
+                if (b == 0)
+                {
+                    x1 = 0;
+                    textBox5.Text = Convert.ToString("");
+                    textBox8.Text = Convert.ToString("");
+                    textBox6.Text = Convert.ToString("");
+                    textBox7.Text = Convert.ToString("");
+                }
+                else
+                {
+                    x1 = -c / b;
+                    textBox5.Text = Convert.ToString(x1);
+                    textBox8.Text = Convert.ToString("");
+                    textBox6.Text = Convert.ToString("");
+                    textBox7.Text = Convert.ToString("");
+                }
+            } else
+
+
+            if (d == 0)
+            {
+
+                DD = b * b - 4 * a * c; 
+                if (DD > 0)
+                {
+                    x1 = (-b + Math.Sqrt(DD)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(DD)) / (2 * a);
+                    textBox5.Text = Convert.ToString(x1);
+                    textBox8.Text = Convert.ToString(x2);
+                    textBox6.Text = Convert.ToString("");
+                    textBox7.Text = Convert.ToString("");
+                }
+                if (DD == 0)
+                {
+                    x1 = -b / (2 * a);
+                    textBox5.Text = Convert.ToString(x1);
+                    textBox8.Text = Convert.ToString("");
+                    textBox6.Text = Convert.ToString("");
+                    textBox7.Text = Convert.ToString("");
+                }
+                if (DD < 0)
+                {
+                    x1 = -b / (2 * a);
+                    x2 = -b / (2 * a);
+                    ix1 = Math.Sqrt(Math.Abs(DD)) / (2 * a);
+                    ix2 = -Math.Sqrt(Math.Abs(DD)) / (2 * a);
+                    textBox5.Text = Convert.ToString(x1);
+                    textBox8.Text = Convert.ToString(x2);
+                    textBox6.Text = Convert.ToString(ix1);
+                    textBox7.Text = Convert.ToString(ix2);
+                }
+                
+                textBox10.Text = Convert.ToString("");                
+                textBox9.Text = Convert.ToString("");
+
+            }
+
+            //if (Math.Abs(d) > 0)
+            else
+            {
+                a = a / d;
+                b = b / d;
+                c = c / d;
+                d = d / d;
+
+
+                Q = (a * a - 3 * b) / 9;
+                R = (2 * a * a * a - 9 * a * b + 27 * c) / 54;
+                S = Q * Q * Q - R * R;
+                if (S > 0)
+                {
+                    phi = Math.Acos(R / Math.Sqrt(Q * Q * Q)) / 3;
+                    x1 = -2 * Math.Sqrt(Q) * Math.Cos(phi) - a / 3;
+                    x2 = -2 * Math.Sqrt(Q) * Math.Cos(phi + 2 * Math.PI / 3) - a / 3;
+                    x3 = -2 * Math.Sqrt(Q) * Math.Cos(phi - 2 * Math.PI / 3) - a / 3;
+                    textBox5.Text = Convert.ToString(x1);
+                    textBox8.Text = Convert.ToString(x2);
+                    textBox10.Text = Convert.ToString(x3);
+                    textBox6.Text = Convert.ToString("");
+                    textBox7.Text = Convert.ToString("");
+                    textBox9.Text = Convert.ToString("");
+                } else
+                if (S < 0)
+                {
+                    if (Q > 0)
+                    {
+                        phi = Math.Abs(R) / Math.Sqrt(Q * Q * Q);
+                        phi = Math.Log(phi + Math.Sqrt(phi * phi - 1)) / 3;
+                        x1 = -2 * Math.Sqrt(Math.Abs(Q)) * Math.Sign(R) * Math.Cosh(phi) - a / 3;
+                        x2 = Math.Sqrt(Math.Abs(Q)) * Math.Sign(R) * Math.Cosh(phi) - a / 3;
+                        x3 = Math.Sqrt(Math.Abs(Q)) * Math.Sign(R) * Math.Cosh(phi) - a / 3;
+                        ix1 = 0;
+                        ix2 = Math.Sqrt(3) * Math.Sqrt(Math.Abs(Q)) * Math.Sinh(phi);
+                        ix3 = -Math.Sqrt(3) * Math.Sqrt(Math.Abs(Q)) * Math.Sinh(phi);
+                        if (Math.Abs(x1) < 1E-16) x1 = 0;
+                        if (Math.Abs(x2) < 1E-16) x2 = 0;
+                        if (Math.Abs(x3) < 1E-16) x3 = 0;
+                        if (Math.Abs(ix1) < 1E-16) ix1 = 0;
+                        if (Math.Abs(ix2) < 1E-16) ix2 = 0;
+                        if (Math.Abs(ix3) < 1E-16) ix3 = 0;
+                        textBox5.Text = Convert.ToString(x1);
+                        textBox8.Text = Convert.ToString(x2);
+                        textBox10.Text = Convert.ToString(x3);
+                        textBox6.Text = Convert.ToString("");
+                        textBox7.Text = Convert.ToString(ix2);
+                        textBox9.Text = Convert.ToString(ix3);
+                    } else
+                    if (Q < 0)
+                    {
+                        phi = Math.Abs(R) / Math.Sqrt(Math.Abs(Q * Q * Q));
+                        phi = Math.Log(phi + Math.Sqrt(phi * phi + 1)) / 3;
+                        x1 = -2 * Math.Sqrt(Math.Abs(Q)) * Math.Sign(R) * Math.Sinh(phi) - a / 3;
+                        x2 = Math.Sqrt(Math.Abs(Q)) * Math.Sign(R) * Math.Sinh(phi) - a / 3;
+                        x3 = Math.Sqrt(Math.Abs(Q)) * Math.Sign(R) * Math.Sinh(phi) - a / 3;
+                        ix1 = 0;
+                        ix2 = Math.Sqrt(3) * Math.Sqrt(Math.Abs(Q)) * Math.Cosh(phi);
+                        ix3 = -Math.Sqrt(3) * Math.Sqrt(Math.Abs(Q)) * Math.Cosh(phi);
+                        if (Math.Abs(x1) < 1E-16) x1 = 0;
+                        if (Math.Abs(x2) < 1E-16) x2 = 0;
+                        if (Math.Abs(x3) < 1E-16) x3 = 0;
+                        if (Math.Abs(ix1) < 1E-16) ix1 = 0;
+                        if (Math.Abs(ix2) < 1E-16) ix2 = 0;
+                        if (Math.Abs(ix3) < 1E-16) ix3 = 0;
+                        textBox5.Text = Convert.ToString(x1);
+                        textBox8.Text = Convert.ToString(x2);
+                        textBox10.Text = Convert.ToString(x3);
+                        textBox6.Text = Convert.ToString("");
+                        textBox7.Text = Convert.ToString(ix2);
+                        textBox9.Text = Convert.ToString(ix3);
+                    } else
+
+                    if (Q == 0)
+                    {
+                        x1 = -Math.Pow((c - a * a * a / 27), 1 / 3);
+                        x2 = -(a + x1) / 2;
+                        x3 = -(a + x1) / 2;
+                        ix1 = 0;
+                        ix2 = 0.5 * Math.Sqrt(Math.Abs((a - 3 * x1) * (a + x1) - 4 * b));
+                        ix3 = -0.5 * Math.Sqrt(Math.Abs((a - 3 * x1) * (a + x1) - 4 * b));
+                        if (Math.Abs(x1) < 1E-16) x1 = 0;
+                        if (Math.Abs(x2) < 1E-16) x2 = 0;
+                        if (Math.Abs(x3) < 1E-16) x3 = 0;
+                        if (Math.Abs(ix1) < 1E-16) ix1 = 0;
+                        if (Math.Abs(ix2) < 1E-16) ix2 = 0;
+                        if (Math.Abs(ix3) < 1E-16) ix3 = 0;
+                        textBox5.Text = Convert.ToString(x1);
+                        textBox8.Text = Convert.ToString(x2);
+                        textBox10.Text = Convert.ToString(x3);
+                        textBox6.Text = Convert.ToString("");
+                        textBox7.Text = Convert.ToString(ix2);
+                        textBox9.Text = Convert.ToString(ix3);
+                    }
+
+                } else
+
+                if (S == 0)
+                {
+                    x1 = -2 * Math.Pow(R, 1.0 / 3) - a / 3;
+                    x2 = Math.Pow(R, 1.0 / 3) - a / 3;
+                    x3 = 0;
+                    ix1 = 0;
+                    ix2 = 0;
+                    ix3 = 0;
+                    if (Math.Abs(x1) < 1E-16) x1 = 0;
+                    if (Math.Abs(x2) < 1E-16) x2 = 0;
+                    if (Math.Abs(x3) < 1E-16) x3 = 0;
+                    if (Math.Abs(ix1) < 1E-16) ix1 = 0;
+                    if (Math.Abs(ix2) < 1E-16) ix2 = 0;
+                    if (Math.Abs(ix3) < 1E-16) ix3 = 0;
+                    textBox5.Text = Convert.ToString(x1);
+                    textBox8.Text = Convert.ToString(x2);
+                    textBox10.Text = Convert.ToString("");
+                    textBox6.Text = Convert.ToString("");
+                    textBox7.Text = Convert.ToString("");
+                    textBox9.Text = Convert.ToString("");
+                }          
+            }
+
+          
+            pictureBox1.Image = null;
+            pictureBox1.Update();
+            pictureBox2.Image = null;
+            pictureBox2.Update();
+
+
+            xmax = 5;
+            xmin = -5;
+            ymax = 5;
+            ymin = -5;
+            y = 0;
+            x0 = xmin; y0 = ymin;
+            while (x0 < xmax) { Point1(x0, 0); x0 = x0 + 0.01; }
+            while (y0 < ymax) { Point1(0, y0); y0 = y0 + 0.01; }
+            x = -10;
+            while (x < 10)
+            {
+                y = d * x * x * x + a * x * x + b * x + c;
+                yy = dd * x * x + aa * x + bb;
+                Point(x, y);
+                Point2(x, yy);
+                x = x + 0.01;
+                
+            }
+
+        }
+
+       
+
+    }
+}
